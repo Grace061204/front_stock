0ef1961e56553a30e8dd835322f50c5067ff0e8e
Hariniaina-Grace <grace.andrianisoa@gmail.com>
2024-09-03 18:53:43 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front_stock',
+  webDir: 'dist'
+};
+
+export default config;
